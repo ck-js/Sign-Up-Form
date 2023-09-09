308daf941477f4153fa35ea1bd47bacb76407373
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-09-09 16:51:35 +0700

    Add password match checking feature that compares two strings and if they match removes the invalid class and adds valid class for both pwd inputs otherwise removes valid class and adds invalid class which means strings dont match yet. The function runs after each input change. The valid class will add a green border where as invalid class adds red border. In summary the pwd input fields have 3 styles depending on validation, the first is that pwd input is neautral upon load which means it looks like every other widget then second state is in focus and empty where red border is applied then after first value entered the input is back to  neautral, then move onto second pwd input field to confirm a match which then the classlist add or remove is triggered after each input on second pwd input field to check for match, if no match then red border applied to both fields then if match both fields have green border.

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ passwordInput1.addEventListener('input', () => {
 passwordInput2.addEventListener('input', () => {
     const input = passwordInput2.value;
     string2 = input;
+    if (string1 === string2) {
+        passwordInput2.classList.remove('invalid');
+        passwordInput1.classList.remove('invalid');
+
+        passwordInput2.classList.add('valid');
+        passwordInput1.classList.add('valid');
+           
+    }else {
+        passwordInput2.classList.remove('valid');
+        passwordInput1.classList.remove('valid');
+
+
+        passwordInput2.classList.add('invalid');
+        passwordInput1.classList.add('invalid');
+    }
+    
 })
 
-if (string1 === string2) {
-    
-}
